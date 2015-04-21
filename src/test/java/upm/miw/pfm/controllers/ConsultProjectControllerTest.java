31bb4832cc9eff2d7ddc588b57b153cd1618c7d2
rodrigolopez <rodrigo@wime.com.mx>
2015-04-21 13:14:14 +0200

    Se cambia el parse que daba error por uno mas seguro en consult test

--- src/test/java/upm/miw/pfm/controllers/ConsultProjectControllerTest.java
+++ src/test/java/upm/miw/pfm/controllers/ConsultProjectControllerTest.java
@@ -4,15 +4,14 @@ import static org.junit.Assert.*;
 
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import upm.miw.pfm.controllers.ejbs.ConsultProjectControllerEjb;
-
 import upm.miw.pfm.mocks.MockProjectDao;
-
 import upm.miw.pfm.models.entities.Project;
 
 public class ConsultProjectControllerTest {
@@ -24,16 +23,12 @@ public class ConsultProjectControllerTest {
 	
 	@Before
 	public void before(){
-		  String startString = "March 2, 2015";
-		  String endString = "September 4, 2015";
-	      DateFormat df = DateFormat.getDateInstance();
 	      try {
-	         start = df.parse(startString);
-	         end = df.parse(endString);
-	      }
-	      catch(ParseException e) {
-	         System.out.println("Unable to parse " + startString);
-	      }
+				start = new SimpleDateFormat("dd-MM-yyyy").parse("02-03-2015");
+				end = new SimpleDateFormat("dd-MM-yyyy").parse("04-09-2015");
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
 		  consultProjectController = new ConsultProjectControllerEjb();
 		  project = new Project("Mi proyecto", start, end, 100000.00);
 		  project.setId(10);
