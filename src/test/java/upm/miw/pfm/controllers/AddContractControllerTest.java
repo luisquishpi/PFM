242b80e1a3a4f760faae678323b9559d9f14497e
Luis Quishpi <luisquishpi @138.4.205.235>
2015-04-21 10:52:11 +0200

    Se crea AddContractControllerTest

---
+++ src/test/java/upm/miw/pfm/controllers/AddContractControllerTest.java
@@ -0,0 +1,19 @@
+package upm.miw.pfm.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AddContractControllerTest {
+
+    @Before
+    public void before(){
+        
+    }
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
