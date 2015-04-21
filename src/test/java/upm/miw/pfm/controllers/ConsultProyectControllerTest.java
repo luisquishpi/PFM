b166c9a47ef197424fbb61177931e748bdffe9a8
rodrigolopez <rodrigo@wime.com.mx>
2015-04-21 09:46:53 +0200

    Se crea test consultar proyecto vacío

---
+++ src/test/java/upm/miw/pfm/controllers/ConsultProyectControllerTest.java
@@ -0,0 +1,14 @@
+package upm.miw.pfm.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConsultProyectControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
