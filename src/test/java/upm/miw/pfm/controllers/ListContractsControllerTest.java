1ea87009176c63bb6cd69781b59b0e2af6bdb561
molinar <jmolinaromero86@gmail.com>
2015-04-22 12:10:12 +0200

    Add ListContractsControllerTest

--- src/test/java/upm/miw/pfm/controllers/ListContractsControllerTest.java
+++ src/test/java/upm/miw/pfm/controllers/ListContractsControllerTest.java
@@ -9,39 +9,37 @@ import org.junit.Before;
 import org.junit.Test;
 
 import upm.miw.pfm.controllers.ejbs.ListContractsControllerEjb;
+import upm.miw.pfm.mocks.MockContractDao;
 import upm.miw.pfm.models.entities.Contract;
 import upm.miw.pfm.utils.ContractType;
 
 public class ListContractsControllerTest {
-	
+
 	private ListContractsController listContractsController;
-	
+
 	@Before
-	public void before(){
+	public void before() {
 		listContractsController = new ListContractsControllerEjb();
 	}
-	
+
 	@Test
-	public void listContractsTest(){
+	public void listContractsTest() {
 		List<Contract> contractList = new ArrayList<Contract>();
-		
+
 		Contract contract1 = new Contract(ContractType.FIJO, 32.5);
 		contractList.add(contract1);
-		
+
 		Contract contract2 = new Contract(ContractType.BECARIO, 2.0);
 		contractList.add(contract2);
 		
+		new MockContractDao(contractList);
 		assertEquals(2, listContractsController.listContracts().size());
 	}
-	
-	/*@Test
-	public void emptyListContractsTest(){
-		List<Contract> contractList = new ArrayList<Contract>();
-		
-		assertEquals(0, listContractsController.listContracts().size());
-	}*/
-	
-	
-	
 
+	@Test 
+	public void emptyListContractsTest(){ 
+	  List<Contract> contractList = new ArrayList<Contract>();
+	  new MockContractDao(contractList);
+	  assertEquals(0, listContractsController.listContracts().size()); 
+	}
 }
