503c76a421403176ee9c9a20ca19cffe1d68bc99
JosePech <jrocks169@gmail.com>
2015-04-22 10:04:41 +0200

    -Quitar archivo sonarquebe -Agrega valores iniciales a constructor sin parametros -Implementa modelo ProjectSchedule -Finaliza Test de SetScheduleController

--- src/main/java/upm/miw/pfm/models/entities/ProjectSchedule.java
+++ src/main/java/upm/miw/pfm/models/entities/ProjectSchedule.java
@@ -3,6 +3,17 @@ package upm.miw.pfm.models.entities;
 public class ProjectSchedule {
     
     private Project project;
+    private Integer workDays;
+    private Double mondayHours;
+    private Double tuesdayHours;
+    private Double wednesdayHours;
+    private Double thursdayHours;    
+    private Double fridayHours;
+    private Double saturdayHours;
+    private Double sundayHours;
+    
+    public ProjectSchedule(){        
+    }
 
     public Project getProject() {
         return this.project;
@@ -12,44 +23,68 @@ public class ProjectSchedule {
         this.project = project;   
     }
 
-    public void setWorkDays(int i) {
-        // TODO Auto-generated method stub
-        
+    public void setWorkDays(Integer i) {
+        this.workDays = i;
     }
 
-    public void setMondayHours(int i) {
-        // TODO Auto-generated method stub
-        
+    public void setMondayHours(Double i) {        
+        this.mondayHours = i;
     }
 
-    public void setTuesdayHours(int i) {
-        // TODO Auto-generated method stub
-        
+    public void setTuesdayHours(Double i) {
+        this.tuesdayHours = i;
     }
 
-    public void setWednesdayHours(int i) {
-        // TODO Auto-generated method stub
-        
+    public void setWednesdayHours(Double i) {
+        this.wednesdayHours = i;
     }
 
-    public void setThursdayHours(int i) {
-        // TODO Auto-generated method stub
-        
+    public void setThursdayHours(Double i) {
+        this.thursdayHours = i;
     }
 
-    public void setFridayHours(int i) {
-        // TODO Auto-generated method stub
-        
+    public void setFridayHours(Double i) {
+        this.fridayHours = i;
     }
 
-    public void setSaturdayHours(int i) {
-        // TODO Auto-generated method stub
-        
+    public void setSaturdayHours(Double i) {
+        this.saturdayHours = i;
     }
 
-    public void setSundayHours(int i) {
-        // TODO Auto-generated method stub
-        
+    public void setSundayHours(Double i) {
+        this.sundayHours = i;
+    }
+
+    public Integer getWorkDays() {
+        return workDays;
+    }
+
+    public Double getMondayHours() {
+        return mondayHours;
+    }
+
+    public Double getTuesdayHours() {
+        return tuesdayHours;
+    }
+
+    public Double getWednesdayHours() {
+        return wednesdayHours;
+    }
+
+    public Double getThursdayHours() {
+        return thursdayHours;
+    }
+
+    public Double getFridayHours() {
+        return fridayHours;
+    }
+
+    public Double getSaturdayHours() {
+        return saturdayHours;
+    }
+
+    public Double getSundayHours() {
+        return sundayHours;
     }
 
 }
--- src/test/java/upm/miw/pfm/controllers/SetScheduleControllerMockTest.java
+++ src/test/java/upm/miw/pfm/controllers/SetScheduleControllerMockTest.java
@@ -10,34 +10,46 @@ import upm.miw.pfm.models.entities.Project;
 import upm.miw.pfm.models.entities.ProjectSchedule;
 
 public class SetScheduleControllerMockTest {
-    
+
     private SetScheduleController controller;
+
+    private static final double DELTA = 1e-15;
 
     @Before
     public void setUp() throws Exception {
         controller = new SetScheduleController();
+        new MockProjectDao();
     }
 
     @Test
     public void SetScheduleTest() {
-        ProjectSchedule schedule = new ProjectSchedule();     
-        
+        ProjectSchedule schedule = new ProjectSchedule();
+
         Project project = new Project();
         project.setId(1);
-                
-        schedule.setWorkDays(21);
-        schedule.setMondayHours(8);
-        schedule.setTuesdayHours(8);
-        schedule.setWednesdayHours(8);
-        schedule.setThursdayHours(8);
-        schedule.setFridayHours(8);
-        schedule.setSaturdayHours(0);
-        schedule.setSundayHours(0);
-        
-        project.setProjectSchedule(schedule);
-        new MockProjectDao();
-        controller.setProjectSchedule(project);        
-        assertEquals(project, controller.getProjectSchedule(1).getProject());
-    }
 
+        schedule.setWorkDays(21);
+        schedule.setMondayHours(8D);
+        schedule.setTuesdayHours(8D);
+        schedule.setWednesdayHours(8D);
+        schedule.setThursdayHours(8D);
+        schedule.setFridayHours(8D);
+        schedule.setSaturdayHours(0D);
+        schedule.setSundayHours(0D);
+
+        project.setProjectSchedule(schedule);
+        
+        controller.setProjectSchedule(project);
+        assertEquals(project, controller.getProjectSchedule(1).getProject());
+        assertEquals(schedule, controller.getProjectSchedule(1));
+        Integer expectedWorkDays = 21;
+        assertEquals(expectedWorkDays, controller.getProjectSchedule(1).getWorkDays());
+        assertEquals(8, controller.getProjectSchedule(1).getMondayHours(), DELTA);
+        assertEquals(8, controller.getProjectSchedule(1).getTuesdayHours(), DELTA);
+        assertEquals(8, controller.getProjectSchedule(1).getWednesdayHours(), DELTA);
+        assertEquals(8, controller.getProjectSchedule(1).getThursdayHours(), DELTA);
+        assertEquals(8, controller.getProjectSchedule(1).getFridayHours(), DELTA);
+        assertEquals(0, controller.getProjectSchedule(1).getSaturdayHours(), DELTA);
+        assertEquals(0, controller.getProjectSchedule(1).getSundayHours(), DELTA);
+    }
 }
