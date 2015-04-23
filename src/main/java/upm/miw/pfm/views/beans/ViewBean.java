e68b59d150c2d182ea6beedfcd035b40d0cc0000
raulvivan <raul.vivanco@opendeusto.es>
2015-04-23 13:35:44 +0200

    Creado ConsultProjectBean y añadidas anotaciones

--- src/main/java/upm/miw/pfm/views/beans/ViewBean.java
+++ src/main/java/upm/miw/pfm/views/beans/ViewBean.java
@@ -1,11 +1,14 @@
 package upm.miw.pfm.views.beans;
 
+import javax.faces.bean.ManagedProperty;
+
 import upm.miw.pfm.controllers.ControllerFactory;
 import upm.miw.pfm.controllers.ejbs.ControllerEjbFactory;
 
 
 public class ViewBean {
 
+	@ManagedProperty(value = "#{controllerFactory}")
     private ControllerFactory controllerFactory=null;
 
     public ControllerFactory getControllerFactory() {
