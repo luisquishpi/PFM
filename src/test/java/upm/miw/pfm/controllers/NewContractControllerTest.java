63ee187060c1d7766b47cd38ac745ff4f24f9ee1
Luis Quishpi <luisquishpi @138.4.205.235>
2015-04-23 10:53:08 +0200

    Update NewContractControllerTest con Mock

--- src/test/java/upm/miw/pfm/controllers/NewContractControllerTest.java
+++ src/test/java/upm/miw/pfm/controllers/NewContractControllerTest.java
@@ -2,12 +2,14 @@ package upm.miw.pfm.controllers;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import upm.miw.pfm.controllers.ejbs.NewContractControllerEjb;
+import upm.miw.pfm.mocks.MockContractDao;
 import upm.miw.pfm.models.entities.Contract;
 import upm.miw.pfm.utils.ContractType;
 
@@ -25,12 +27,19 @@ public class NewContractControllerTest {
     public void testNewContract() {
         Contract contract = new Contract(ContractType.FIJO, 32.5);
         contractControllerEjb.saveContract(contract);
-        List<Contract> contractList= contractControllerEjb.contractList();
-        assertEquals(2, contractList.size());
-        assertTrue(contractControllerEjb.existContract(contract));
+
+        List<Contract> contractList = new ArrayList<Contract>();
+        contractList.add(contract);
+        new MockContractDao(contractList);
+        contractList = contractControllerEjb.contractList();
+
+        assertEquals(1, contractList.size());
     }
+
     @Test
     public void testExistContract() {
+        Contract contract = new Contract(ContractType.BECARIO, 32.5);
+        new MockContractDao(contract);
         assertTrue(contractControllerEjb.existContract(new Contract(ContractType.BECARIO, 32.5)));
     }
 }
