afff625c721c2c1c01e66729a01b443e8e5de262
cpalacios <c.palaciosc@alumnos.upm.es>
2015-04-24 10:45:54 +0200

    Agregar soporte EJB para los controladores

--- src/main/java/upm/miw/pfm/views/beans/ListProjectBean.java
+++ src/main/java/upm/miw/pfm/views/beans/ListProjectBean.java
@@ -3,26 +3,27 @@ package upm.miw.pfm.views.beans;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 
+import upm.miw.pfm.controllers.ListProjectController;
 import upm.miw.pfm.models.entities.Project;
 
 @ManagedBean
 public class ListProjectBean extends ViewBean {
 
 	private List<Project> projects;
+	
+    @EJB
+    private ListProjectController listProjectController;	
 
 	public List<Project> getProjects() {
 		return projects;
 	}
-
-	public void setProjects(List<Project> projects) {
-		this.projects = projects;
-	}
-
+	
 	@PostConstruct
 	public void update() {
-		projects = this.getControllerFactory().getListProjectController().listProjects();
+		projects = listProjectController.listProjects();
 	}
 
 }
