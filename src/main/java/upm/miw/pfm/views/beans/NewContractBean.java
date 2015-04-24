b78622943ee85feff359579a2dd8316034b81257
Luis Quishpi <luisquishpi @138.4.205.235>
2015-04-24 12:08:40 +0200

    Se agrega anotaciones @EJB a los BEANS y se deja de utilizar la factoria de controladores

--- src/main/java/upm/miw/pfm/views/beans/NewContractBean.java
+++ src/main/java/upm/miw/pfm/views/beans/NewContractBean.java
@@ -1,24 +1,25 @@
 package upm.miw.pfm.views.beans;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 
+import upm.miw.pfm.controllers.NewContractController;
 import upm.miw.pfm.models.entities.Contract;
 @ManagedBean
 public class NewContractBean extends ViewBean {
 
     private Contract contract;
-
+    @EJB
+    private NewContractController newContractsController;
     
     public Contract getContract() {
         return contract;
     }
-
     public void setContract(Contract contract) {
         this.contract = contract;
     }
-    
     public String process() {
-        this.getControllerFactory().getNewContractController().saveContract(contract);
+        newContractsController.saveContract(contract);
         return "home";
     }
 
