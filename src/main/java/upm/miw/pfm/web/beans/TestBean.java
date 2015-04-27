80b528dcdff0bd60273105da278921374fc0ec45
JosePech <jrocks169@gmail.com>
2015-04-27 13:00:45 +0200

    TestBean prueba angularjs

--- src/main/java/upm/miw/pfm/web/beans/TestBean.java
+++ src/main/java/upm/miw/pfm/web/beans/TestBean.java
@@ -7,7 +7,7 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean
 public class TestBean implements Serializable {		
 	private static final long serialVersionUID = 1L;
-		private Integer laborDay=100;
+		private Integer laborDay=100; 
 
 		public Integer getLaborDay() {
 			return laborDay;
