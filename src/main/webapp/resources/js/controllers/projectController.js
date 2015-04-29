356e1c7f9fda9bfdf6ed1056b511036eb130bc61
rodrigolopez <rodrigo@wime.com.mx>
2015-04-29 16:07:16 +0200

    Se corrige problema de digest en el controlador de proyecto de angular.

--- src/main/webapp/resources/js/controllers/projectController.js
+++ src/main/webapp/resources/js/controllers/projectController.js
@@ -5,42 +5,38 @@ projectApp.controller(
 	  'projectController', function ($scope, test) {  
 	  
 	  !test && initJSFScope($scope);
+	  var start;
+	  var end;
 	
 	  $scope.projectBean.project.start = moment(new Date($scope.projectBean.project.start));
 	  $scope.projectBean.project.end = moment(new Date($scope.projectBean.project.end));
 	  
 	  $scope.workDays = 0;
-	  /*
-	  $scope.$watchGroup(['start', 'end'], function(newValues, oldValues, scope) {
+	  $scope.naturalDays = 0;
+	  
+	  $scope.$watchGroup(['projectBean.project.start', 'projectBean.project.end'], function(newValues, oldValues, scope) {
 		  var days=0;
 		  start = $scope.projectBean.project.start;
-		  end = $scope.projectBean.project.end;		  
+		  end = $scope.projectBean.project.end;		
 		  for (var m = moment(start); m.isBefore(end); m.add(1, 'days')) {
 			  if(($scope.workingDays).indexOf(m.day())>-1)
 				  days++	
 			}
-		  $scope.workDays = 32;
+		  $scope.workDays = days;
+		  
+		  $scope.naturalDays= end.add(1,'days').diff(start,'days');
 	  });
-	  */
-	  	  
-	  var start;
-	  var end;
-	  $scope.naturalDays = function(){
-		  start = $scope.projectBean.project.start;
-		  end = $scope.projectBean.project.end;
-		  return end.add(1,'days').diff(start,'days');
-	  }
 	  
 	  $scope.naturalMonths = function(){
-		  return $scope.naturalDays()/30;
+		  return $scope.naturalDays/30;
 	  }
 	  
 	  $scope.workMonths = function(){
-		  return $scope.workDays()/$scope.projectBean.projectSchedule.workDays;
+		  return $scope.workDays/$scope.projectBean.projectSchedule.workDays;
 	  }
 	  	  
 	  $scope.workHours = function(){
-		  return 0;
+		  return 1;
 	  }
 	  
 	  $scope.costNaturalMonth = function(){
@@ -48,7 +44,7 @@ projectApp.controller(
 	  }
 	  
 	  $scope.costNaturalDay = function(){
-		  return (($scope.projectBean.project.cost/$scope.naturalDays()).toFixed(2)/1);
+		  return (($scope.projectBean.project.cost/$scope.naturalDays).toFixed(2)/1);
 	  }
 	  
 	  $scope.costWorkMonth = function(){
@@ -56,7 +52,7 @@ projectApp.controller(
 	  }
 	  
 	  $scope.costWorkDay = function(){
-		  return $scope.projectBean.project.cost/$scope.workDays();
+		  return $scope.projectBean.project.cost/$scope.workDays;
 	  }
 	  
 	  $scope.costWorkHour = function(){
