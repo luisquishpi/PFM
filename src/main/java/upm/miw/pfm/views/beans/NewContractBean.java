11143d2be4e812ecb7c3db3ffe34866cafa1affa
Luis Quishpi <luisquishpi @138.4.205.235>
2015-04-29 12:00:13 +0200

    Interface de Contract: Index , New

--- src/main/java/upm/miw/pfm/views/beans/NewContractBean.java
+++ src/main/java/upm/miw/pfm/views/beans/NewContractBean.java
@@ -2,25 +2,47 @@ package upm.miw.pfm.views.beans;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.model.SelectItem;
+
+import org.apache.logging.log4j.LogManager;
 
 import upm.miw.pfm.controllers.NewContractController;
 import upm.miw.pfm.models.entities.Contract;
+import upm.miw.pfm.utils.ContractType;
+
 @ManagedBean
 public class NewContractBean extends ViewBean {
 
     private Contract contract;
+
     @EJB
     private NewContractController newContractsController;
-    
+
     public Contract getContract() {
         return contract;
     }
+
     public void setContract(Contract contract) {
         this.contract = contract;
     }
+
     public String process() {
         newContractsController.saveContract(contract);
+        LogManager.getLogger(this).info("Guardado " + contract);
         return "home";
+    }
+
+    public SelectItem[] getContractTypeValues() {
+        SelectItem[] items = new SelectItem[ContractType.values().length];
+        int i = 0;
+        for (ContractType g : ContractType.values()) {
+            items[i++] = new SelectItem(g, g.toString());
+            System.out.println("item:" + items[i++]);
+        }
+        return items;
+    }
+    public ContractType[] getListContractType(){
+        return ContractType.values();
     }
 
 }
