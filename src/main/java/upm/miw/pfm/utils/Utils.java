b6728bdb2bf4ab63d91d9871e9ffed07cf1f836a
cpalacios <c.palaciosc@alumnos.upm.es>
2015-04-30 02:22:49 +0200

    Creación de proyecto

--- src/main/java/upm/miw/pfm/utils/Utils.java
+++ src/main/java/upm/miw/pfm/utils/Utils.java
@@ -1,5 +1,7 @@
 package upm.miw.pfm.utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -8,9 +10,29 @@ public class Utils {
     public static Date buildDate(int year, int month, int day) {
         return new Calendar.Builder().setDate(year, month, day).build().getTime();
     }
-    
-    public static Date now(){
-        return Calendar.getInstance().getTime();
+
+    public static Date now(String format) {
+        return Utils.format(Calendar.getInstance().getTime(), format);
+    }
+
+    public static Date addDaysToNow(int days, String format) {
+        Calendar now = Calendar.getInstance();
+        now.add(Calendar.DAY_OF_MONTH, days);
+        return Utils.format(now.getTime(), format);
+    }
+
+    public static Date format(Date date, String format) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        try {
+            return sdf.parse(sdf.format(date));
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+    public static String formatString(Date date, String format) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(date);
     }
 
 }
