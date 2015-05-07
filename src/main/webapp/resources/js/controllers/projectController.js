8894384f51f744edbf61111297f568baaf4ffd42
rodrigolopez <rodrigo@wime.com.mx>
2015-05-07 09:14:53 +0200

    Se corrije problema de los test de travis con angular-jasmine

--- src/main/webapp/resources/js/controllers/projectController.js
+++ src/main/webapp/resources/js/controllers/projectController.js
@@ -2,20 +2,18 @@
  * AngularJS ProjectController
  */
 projectApp.controller("projectController", function ($scope, $isTest) {  
-	if(!$isTest){
+	  if(!$isTest){
 		  initJSFScope($scope);
 	  }
-	  var start;
-	  var end;
+	  var start, end;
 	  	  
 	  $scope.workDays = 0;
 	  $scope.workHours = 0;
 	  $scope.naturalDays = 0;
 	  
-	  $scope.initForTest = function(){
-		  var days=0;
-		  var hours=0;
-		  
+	  function calculateWorkDaysAndHour(){
+		  var days=0, hours=0;
+ 
 		  start = moment($scope.projectBean.project.startString, "DD/MM/YYYY");
 		  end = moment($scope.projectBean.project.endString, "DD/MM/YYYY");
 		  end = end.add(1,"days");
@@ -30,32 +28,22 @@ projectApp.controller("projectController", function ($scope, $isTest) {
 		  $scope.workHours = hours;
 		  $scope.naturalDays= end.diff(start,"days");
 	  }
-
-	  $scope.$watchGroup(["projectBean.project.startString", "projectBean.project.endString"], function(newValues, oldValues, scope) {
-		  var days=0;
-		  var hours=0;
-		  
-		  start = moment($scope.projectBean.project.startString, "DD/MM/YYYY");
-		  end = moment($scope.projectBean.project.endString, "DD/MM/YYYY");
-		  end = end.add(1,"days");
-		  for (var m = moment(start); m.isBefore(end); m.add(1, "days")) {
-			  var workHours = $scope.workingDays[m.day()].workHours;
-			  if(workHours>0){
-				  days++;
-			  	  hours+=workHours;
-			  }
-			}
-		  $scope.workDays = days;
-		  $scope.workHours = hours;
-		  $scope.naturalDays= end.diff(start,"days");
-	  });
 	  
+	  $scope.initForTest = function(){
+		  calculateWorkDaysAndHour();
+	  }
+	  
+	  if(!$isTest){
+		  $scope.$watchGroup(["projectBean.project.startString", "projectBean.project.endString"], function(newValues, oldValues, scope) {
+			  calculateWorkDaysAndHour();
+		  });
+	  }
+	  	  
 	  $scope.naturalMonths = function(){
 		  return $scope.naturalDays/30;
 	  }
 	  
 	  $scope.workMonths = function(){
-		  console.log($scope.workDays);
 		  return $scope.workDays/$scope.projectBean.projectSchedule.workDays;
 	  }
 	  	  	  
