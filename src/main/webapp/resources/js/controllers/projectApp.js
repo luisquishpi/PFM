3333477e4b3249094b77f5389c6e858b9aba1993
JosePech <jrocks169@gmail.com>
2015-05-07 11:57:01 +0200

    -Archivo de configuracion de el modulo de angularjs

---
+++ src/main/webapp/resources/js/controllers/projectApp.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+app.value("test", false);
+var projectApp = angular.module('projectApp', [ "angularfaces" ]);
+
+projectApp.factory('broadcastService', function($rootScope) {
+    var sharedService = {};
+    
+    sharedService.data = '';
+
+    sharedService.prepForBroadcast = function(msg) {
+        this.data = msg;
+        this.broadcastItem();
+    };
+
+    sharedService.broadcastItem = function() {
+        $rootScope.$broadcast('handleBroadcast');
+    };
+
+    return sharedService;
+});
