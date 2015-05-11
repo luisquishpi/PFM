b682e6db188be094a564c4190034e2e20f5e4155
Luis Quishpi <luisquishpi @138.4.205.235>
2015-05-11 10:29:46 +0200

    Controladores de Vacaciones

--- src/main/java/upm/miw/pfm/models/daos/hibernate/DaoHibernateFactory.java
+++ src/main/java/upm/miw/pfm/models/daos/hibernate/DaoHibernateFactory.java
@@ -6,27 +6,33 @@ import upm.miw.pfm.models.daos.DaoFactory;
 import upm.miw.pfm.models.daos.EmployeeDao;
 import upm.miw.pfm.models.daos.ProjectDao;
 import upm.miw.pfm.models.daos.ProjectScheduleDao;
+import upm.miw.pfm.models.daos.VacationDao;
 
-public class DaoHibernateFactory extends DaoFactory  {
+public class DaoHibernateFactory extends DaoFactory {
 
-	@Override
-	public ProjectDao getProjectDao() {
-		return new ProjectDaoHibernate();
-	}
+    @Override
+    public ProjectDao getProjectDao() {
+        return new ProjectDaoHibernate();
+    }
 
-	@Override
-	public ContractDao getContractDao() {
-		return new ContractDaoHibernate();
-	}
+    @Override
+    public ContractDao getContractDao() {
+        return new ContractDaoHibernate();
+    }
 
-	@Override
-	public EmployeeDao getEmployeeDao() {
-		return new EmployeeDaoHibernate();
-	}
+    @Override
+    public EmployeeDao getEmployeeDao() {
+        return new EmployeeDaoHibernate();
+    }
 
-	@Override
-	public ProjectScheduleDao getProjectScheduleDao() {
-		return new ProjectScheduleDaoHibernate();
-	}
+    @Override
+    public ProjectScheduleDao getProjectScheduleDao() {
+        return new ProjectScheduleDaoHibernate();
+    }
+
+    @Override
+    public VacationDao getVacationDao() {
+        return new VacationDaoHibernate();
+    }
 
 }
