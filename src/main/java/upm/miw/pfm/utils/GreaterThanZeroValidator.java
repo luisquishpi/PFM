1b635e1c39f73bae8306dc95f6dcde8c02224bdb
cpalacios <c.palaciosc@alumnos.upm.es>
2015-05-12 10:29:04 +0200

    Modificaciones a la pagina de mostrar fases teoricas. Creacion de validadores para combobox y numeros mayores a 0.

---
+++ src/main/java/upm/miw/pfm/utils/GreaterThanZeroValidator.java
@@ -0,0 +1,33 @@
+package upm.miw.pfm.utils;
+
+import java.math.BigDecimal;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator("upm.miw.pfm.utils.GreaterThanZeroValidator")
+public class GreaterThanZeroValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value)
+            throws ValidatorException {
+        try {
+            if (new BigDecimal(value.toString()).signum() < 1) {
+                FacesMessage msg = new FacesMessage("Error de validación.",
+                        "Número debe ser mayor a 0");
+                msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+                throw new ValidatorException(msg);
+            }
+        } catch (NumberFormatException ex) {
+            FacesMessage msg = new FacesMessage("Error de validación.", "Valor ingresado no es un número");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(msg);
+        }
+
+    }
+
+}
--- src/main/java/upm/miw/pfm/views/beans/ShowTheoreticalPhasesBean.java
+++ src/main/java/upm/miw/pfm/views/beans/ShowTheoreticalPhasesBean.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.event.AjaxBehaviorEvent;
 
 import org.apache.logging.log4j.LogManager;
 
 import upm.miw.pfm.controllers.ProjectController;
 import upm.miw.pfm.models.entities.Project;
+import upm.miw.pfm.utils.Utils;
 
 @ManagedBean
 public class ShowTheoreticalPhasesBean {
@@ -49,14 +52,26 @@ public class ShowTheoreticalPhasesBean {
         this.projectList = projectList;
     }
 
-    public String defineIterationDays() {
-        if (this.project.getId() != null && this.project.getId()  != -1) {
-            Project projectToUpdate = projectController.getProject(this.project.getId());
+    public void onChangeProject(AjaxBehaviorEvent e) {
+        this.project = findSelectedProject();
+        LogManager.getLogger(clazz).debug("Proyecto seleccionado " + this.project);
+
+    }
+
+    public String process() {
+        if (this.project.getId() != null && this.project.getId() != -1) {
+            Project projectToUpdate = findSelectedProject();
             projectToUpdate.setIterationDays(this.project.getIterationDays());
             projectController.updateProject(projectToUpdate);
             LogManager.getLogger(clazz).debug("Proyecto a actualizar " + projectToUpdate);
+            Utils.addMessage(FacesMessage.SEVERITY_INFO, "Proyecto",
+                    "Se actualizó las fases teóricas satisfactoriamente");
         }
-        return null;
+        return "index";
     }
-
+    
+    private Project findSelectedProject(){
+        return projectController.getProject(this.project.getId());
+    }
+    
 }
