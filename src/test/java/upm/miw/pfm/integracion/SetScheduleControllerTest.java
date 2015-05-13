70ddfe4fa29229508585484ed64be05824896093
molinar <jmolinaromero86@gmail.com>
2015-05-13 13:21:06 +0200

    Validación de campos en interfaz de crear proyecto

--- src/test/java/upm/miw/pfm/integracion/SetScheduleControllerTest.java
+++ src/test/java/upm/miw/pfm/integracion/SetScheduleControllerTest.java
@@ -1,11 +1,9 @@
 package upm.miw.pfm.integracion;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,7 +15,6 @@ import upm.miw.pfm.controllers.ejbs.SetScheduleControllerEjb;
 import upm.miw.pfm.models.entities.Project;
 import upm.miw.pfm.models.entities.ProjectSchedule;
 import upm.miw.pfm.utils.Utils;
-import upm.miw.pfm.utils.WorkDay;
 
 public class SetScheduleControllerTest {
 
@@ -72,12 +69,5 @@ public class SetScheduleControllerTest {
                 .getSaturdayHours(), DELTA);
         assertEquals(0, setScheduleController.getProjectSchedule(project.getId()).getSundayHours(),
                 DELTA);
-
-        List<WorkDay> workdays = setScheduleController.getProjectSchedule(project.getId()).getWorkDaysArray();
-        for (int i = 1; i < 6; i++) {
-            assertTrue(workdays.get(i).getWorkHours() > 0);
-        }
-        assertTrue(workdays.get(0).getWorkHours() == 0);
-        assertTrue(workdays.get(6).getWorkHours() == 0);
     }
 }
