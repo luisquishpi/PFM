3828ed53e31984f3e2e4010c37c776d64e43d44a
molinar <jmolinaromero86@gmail.com>
2015-06-03 09:42:36 +0200

    Controlador theoricalDisciplineController

--- src/main/webapp/resources/js/controllers/theoricalDisciplineController.js
+++ src/main/webapp/resources/js/controllers/theoricalDisciplineController.js
@@ -114,4 +114,173 @@ projectApp.controller("theoricalDisciplineController", ['$scope', '$isTest', 'Pe
 	$scope. = function(){
 		return
 	}
+	
+	
+	
+	
+	/***********************Personas-mes*************************/
+	//Fase Inicio
+	$scope.initialProjectManagmentMonth = function(){
+		return 0.23;
+	}
+	
+	$scope.initialRequirementsMonth = function(){
+		return 0.63;
+	}
+	
+	$scope.initialAnalysisMonth = function(){
+		return 0.31;
+	}
+	
+	$scope.initialImplementationMonth = function(){
+		return 0.13;
+	}
+	
+	$scope.initialTestsMonth = function(){
+		return 0.13;
+	}
+	
+	$scope.initialDeploymentMonth = function(){
+		return 0.05;
+	}
+	
+	$scope.initialVersionMonth = function(){
+		return 0.16;
+	}
+	
+	$scope.totalInitialMonth = function(){
+		return 1.6;
+	}
+	
+	//Fase Elaboración
+	$scope.elaborationProjectManagmentMonth = function(){
+		return 0.79;
+	}
+	
+	$scope.elaborationRequirementsMonth = function(){
+		return 1.19;
+	}
+	
+	$scope.elaborationAnalysisMonth = function(){
+		return 2.37;
+	}
+	
+	$scope.elaborationImplementationMonth = function(){
+		return 0.86;
+	}
+	
+	$scope.elaborationTestsMonth = function(){
+		return 0.66;
+	}
+	
+	$scope.elaborationDeploymentMonth = function(){
+		return 0.2;
+	}
+	
+	$scope.elaborationVersionMonth = function(){
+		return 0.53;
+	}
+	
+	$scope.totalElaborationMonth = function(){
+		return 6.6;
+	}
+	
+	//Fase Construcción
+	$scope.constructionProjectManagmentMonth = function(){
+		return 2.14;
+	}
+	
+	$scope.constructionRequirementsMonth = function(){
+		return 1.71;
+	}
+	
+	$scope.constructionAnalysisMonth = function(){
+		return 3.42;
+	}
+	
+	$scope.constructionImplementationMonth = function(){
+		return 7.28;
+	}
+	
+	$scope.constructionTestsMonth = function(){
+		return 5.14;
+	}
+	
+	$scope.constructionDeploymentMonth = function(){
+		return 0.64;
+	}
+	
+	$scope.constructionVersionMonth = function(){
+		return 1.07;
+	}
+	
+	$scope.totalConstructionMonth = function(){
+		return 21.4;
+	}
+	
+	//Fase Transición
+	$scope.transitionProjectManagmentMonth = function(){
+		return 0.46;
+	}
+	
+	$scope.transitionRequirementsMonth = function(){
+		return 0.13;
+	}
+	
+	$scope.transitionAnalysisMonth = function(){
+		return 0.13;
+	}
+	
+	$scope.transitionImplementationMonth = function(){
+		return 0.63;
+	}
+	
+	$scope.transitionTestsMonth = function(){
+		return 0.79;
+	}
+	
+	$scope.transitionDeploymentMonth = function(){
+		return 0.99;
+	}
+	
+	$scope.transitionVersionMonth = function(){
+		return 0.16;
+	}
+	
+	$scope.totalTransitionMonth = function(){
+		return 3.3;
+	}
+	
+	//Fase Proyecto
+	$scope.projectProjectManagmentMonth = function(){
+		return 3.62;
+	}
+	
+	$scope.projectRequirementsMonth = function(){
+		return 3.65;
+	}
+	
+	$scope.projectAnalysisMonth = function(){
+		return 6.24;
+	}
+	
+	$scope.projectImplementationMonth = function(){
+		return 8.89;
+	}
+	
+	$scope.projectTestsMonth = function(){
+		return 6.72;
+	}
+	
+	$scope.projectDeploymentMonth = function(){
+		return 1.88;
+	}
+	
+	$scope.projectVersionMonth = function(){
+		return 1.93;
+	}
+	
+	$scope.totalProjectMonth = function(){
+		return 32.9;
+	}
 }]);
