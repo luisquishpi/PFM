cd3c51a640efca64edac69b38cb712bb3ad03800
raulvivan <raul.vivanco@opendeusto.es>
2015-06-10 12:22:36 +0200

    Añadido test de hoursRolePhase

--- src/test/java/upm/miw/pfm/models/daos/AllTests.java
+++ src/test/java/upm/miw/pfm/models/daos/AllTests.java
@@ -4,9 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+
 @RunWith(Suite.class)
 @SuiteClasses({ContractDaoTest.class, EmployeeDaoTest.class, ProjectDaoTest.class,
-        ProjectScheduleDaoTest.class, HolidayDaoTest.class})
+        ProjectScheduleDaoTest.class, HolidayDaoTest.class, HoursRolePhaseDaoTest.class})
 public class AllTests {
 
 }
