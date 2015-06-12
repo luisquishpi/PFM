cd9608ae47c8db05aa9fd76ec1aeef9828e673f8
molinar <jmolinaromero86@gmail.com>
2015-06-12 12:28:51 +0200

    Refactorización Bean disciplinas y fases

--- src/main/java/upm/miw/pfm/rest/EmployeeURI.java
+++
@@ -1,9 +0,0 @@
-package upm.miw.pfm.rest;
-
-public interface EmployeeURI {
-	
-	final String PATH_EMPLOYEES = "/Employees";
-	
-	final String PATH_SAVE= "/Save";
-
-}
--- src/main/java/upm/miw/pfm/views/beans/ShowTheoricalPhasesBean.java
+++ src/main/java/upm/miw/pfm/views/beans/ShowTheoricalPhasesBean.java
@@ -1,110 +1,23 @@
 package upm.miw.pfm.views.beans;
 
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.event.ValueChangeEvent;
 
 import org.apache.logging.log4j.LogManager;
-
-import upm.miw.pfm.controllers.EmployeeController;
-import upm.miw.pfm.controllers.ProjectController;
-import upm.miw.pfm.controllers.SetScheduleController;
-import upm.miw.pfm.models.entities.Employee;
 import upm.miw.pfm.models.entities.Project;
-import upm.miw.pfm.models.entities.ProjectSchedule;
 import upm.miw.pfm.utils.Utils;
 
 @ManagedBean
 @ViewScoped
-public class ShowTheoricalPhasesBean {
-
-    private Project project;
-
-    private ProjectSchedule projectSchedule;
-
-    private List<Project> projectList;
-
-    private List<Employee> employeeList;
-
-    private boolean emptyProject;
-
-    private final static Class<ListProjectsBean> clazz = ListProjectsBean.class;
-
-    @EJB
-    private ProjectController projectController;
-
-    @EJB
-    private EmployeeController employeeController;
-
-    @EJB
-    private SetScheduleController setScheduleController;
-
-    public ShowTheoricalPhasesBean() {
-        this.project = new Project();
-        this.projectSchedule = new ProjectSchedule();
-        this.project.setId(-1);
-        this.project.setCost(0.00);
-        this.project.setStart(Utils.now(Utils.DD_MM_YYYY_FORMAT));
-        this.project.setEnd(Utils.now(Utils.DD_MM_YYYY_FORMAT));
-        this.project.setIterationDays(0);
-        this.emptyProject = true;
-    }
-
-    @PostConstruct
-    public void init() {
-        projectList = projectController.listProjects();
-        LogManager.getLogger(clazz).info("Se encontraron " + projectList.size() + " proyectos");
-
-        employeeList = employeeController.listEmployees();
-        LogManager.getLogger(clazz).info("Se encontraron " + employeeList.size() + " empleados");
-    }
-
-    public Project getProject() {
-        return project;
-    }
-
-    public ProjectSchedule getProjectSchedule() {
-        return projectSchedule;
-    }
-
-    public List<Project> getProjectList() {
-        return projectList;
-    }
-
-    public List<Employee> getEmployeeList() {
-        return employeeList;
-    }
-
-    public void setEmployeeList(List<Employee> employeeList) {
-        this.employeeList = employeeList;
-    }
-
-    public void onChangeProject(ValueChangeEvent e) {
-        Integer selectedProject = (Integer) e.getNewValue();
-        LogManager.getLogger(clazz).debug("Id de proyecto seleccionado " + selectedProject);
-        if (selectedProject != -1) {
-            this.project.setId(selectedProject);
-            this.project = findSelectedProject();
-            this.projectSchedule = setScheduleController.getProjectSchedule(project.getId());
-            LogManager.getLogger(clazz).debug("Proyecto cargado " + this.project);
-            LogManager.getLogger(clazz).info("Project schedule asociado " + this.projectSchedule);
-            this.emptyProject = false;
-        } else {
-            this.emptyProject = true;
-        }
-        FacesContext.getCurrentInstance().renderResponse();
-    }
-
+public class ShowTheoricalPhasesBean extends DisciplinesPhasesBean{
+	
+	private DisciplinesPhasesBean phasesBean;
+	
     public String process() {
-        if (this.project.getId() != null && this.project.getId() != -1) {
+        if (phasesBean.getProject().getId() != null && phasesBean.getProject().getId() != -1) {
             Project projectToUpdate = findSelectedProject();
-            projectToUpdate.setIterationDays(this.project.getIterationDays());
+            projectToUpdate.setIterationDays(phasesBean.getProject().getIterationDays());
             projectController.updateProject(projectToUpdate);
             LogManager.getLogger(clazz).debug("Proyecto a actualizar " + projectToUpdate);
             Utils.addMessage(FacesMessage.SEVERITY_INFO, "Proyecto",
@@ -112,13 +25,4 @@ public class ShowTheoricalPhasesBean {
         }
         return "show_theorical_phases";
     }
-
-    private Project findSelectedProject() {
-        return projectController.getProject(this.project.getId());
-    }
-
-    public boolean isEmptyProject() {
-        return emptyProject;
-    }
-
 }
