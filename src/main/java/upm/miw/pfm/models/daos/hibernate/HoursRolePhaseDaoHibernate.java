ba5205a3c30259fef7ea839a12d42bd9704fcdb7
cpalacios <c.palaciosc@alumnos.upm.es>
2015-06-18 13:27:13 +0200

    Consulta de recursos de un proyecto en ResourcesBean Creacion de controlador y metodo dao Creacion de test dao

--- src/main/java/upm/miw/pfm/models/daos/hibernate/HoursRolePhaseDaoHibernate.java
+++ src/main/java/upm/miw/pfm/models/daos/hibernate/HoursRolePhaseDaoHibernate.java
@@ -1,7 +1,15 @@
 package upm.miw.pfm.models.daos.hibernate;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 import upm.miw.pfm.models.daos.HoursRolePhaseDao;
 import upm.miw.pfm.models.entities.HoursRolePhase;
+import upm.miw.pfm.models.entities.Project;
+import upm.miw.pfm.utils.HibernateUtil;
 
 public class HoursRolePhaseDaoHibernate extends GenericDaoHibernate<HoursRolePhase, Integer> implements
 HoursRolePhaseDao {
@@ -20,5 +28,28 @@ HoursRolePhaseDao {
 	public void deleteAll() {
 		query("delete from HoursRolePhase");
 	}
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<HoursRolePhase> findByProject(Project project) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        List<HoursRolePhase> result = new ArrayList<HoursRolePhase>();
+        try {
+            session.beginTransaction();
+            Query query = session
+                    .createQuery("from HoursRolePhase h WHERE h.project = :project ");
+            query.setParameter("project", project);
+            result = query.list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return result;
+    }
 
 }
